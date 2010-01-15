6508d1f0ab0bf87fa5b85875c8d1849388994031
gbabun <gbabun@c8624efe-c7c6-11de-91be-c712730c8c03>
2010-01-15 11:34:47 +0000

    update od stats

--- backend/stats.py
+++ backend/stats.py
@@ -13,10 +13,30 @@ from google.appengine.ext.webapp import template
 
 class VisualStats(webapp.RequestHandler):
 	def get(self):
-		template_variables=''
+		#dailyStats = self.latestDailyStats()
+		dailyStats = [ '2010-01-14', '2010-01-13','2010-01-12' ]
+		weeklyStats = [ '2010-01-12', '2010-01-05','2009-12-29' ]
+		#weeklyStats = self.latestWeeklyStats()
+		template_variables={'dailyStats':dailyStats, 'weeklyStats':weeklyStats}
 		logging.info('generating visual stats')
 		path= os.path.join(os.path.dirname(__file__), 'templates/stats.html')
 		self.response.out.write(template.render(path,template_variables))
+	def latestDailyStats(self):
+		allDailyStats = DailyDomainStats.getAll()
+		allDailyStatsDates = [ s.date for d in allDailyStats if s.date is not None ]
+		uniqDates = set(alldailyStatsDates)
+		maxDates = len(uniqDates) 
+		if maxDates > 7:
+			maxDates = 7
+		top7Dates = set(uniqDates)[1:maxDates]
+	def latestWeeklyStats(self):
+		allWeeklyStats = WeeklyDomainStats.getAll()
+		allWeeklyStatsDates = [ s.date for d in allWeeklyStats if s.date is not None ]
+		uniqDates = set(allWeeklyStatsDates)
+		maxDates = len(uniqDates) 
+		if maxDates > 7:
+			maxDates = 7
+		top7Dates = set(uniqDates)[1:maxDates]
 
 application = webapp.WSGIApplication(
                                      [('/stats', VisualStats)],
