defe4e9ae70edcd99f08a10994b6fae0d0fd6ea1
gbabun <gbabun@c8624efe-c7c6-11de-91be-c712730c8c03>
2010-02-03 16:57:54 +0000

    init chrome.manifest

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd $HOME/d/work/_ff_extensions/instaright/
 zip -vr chrome/instaright.zip content/ skin/  -x "*.svn*"
 mv chrome/instaright.zip chrome/instaright.jar
-zip -vr instaright.zip chrome/ install.js  install.rdf defaults/   -x "*.svn*"
+zip -vr instaright.zip chrome/ install.js  install.rdf chrome.manifest defaults/   -x "*.svn*"
 mv instaright.zip instaright.xpi
