7c1d6148f3248c630487d91bab3e32534d0f997f
gbabun <gbabun@c8624efe-c7c6-11de-91be-c712730c8c03>
2010-07-05 21:40:22 +0000

    data consolidation update

--- backend/data_consolidation.py
+++ backend/data_consolidation.py
@@ -8,21 +8,22 @@ from google.appengine.ext.webapp import template
 from utils import StatsUtil
 from main import SessionModel
 from models import UserLocationModel, CityStats, CountryStats, LinkStats
-class DomainConsolidation(webapp.RequestHandler):
+class GeneralConsolidation(webapp.RequestHandler):
 	def get(self):
 		memcache_key = 'domain_update_key'
 		message = []
-		generalConsolidation = GeneralConsolidation()
 		#lower_limit_date = datetime.datetime.strptime('2009-11-15', '%Y-%m-%d').date()
 		upper_limit_date = datetime.datetime.today().date()
 		dateStr = self.request.get('date',None)
-		if dateStr is None: 
+		processType = self.request.get('process_type',None)
+		if dateStr is None and processType is None: 
 			logging.info('no date specified')
 			return
 		date = datetime.datetime.strptime(dateStr, "%Y-%m-%d").date()
 		if  upper_limit_date - date < datetime.timedelta(1):
 			logging.info('invalid date specified: %s' %dateStr)
 			self.response.out.write('invalid date specified: %s' %dateStr)
+			memcache.delete(memcache_key)
 			return
 		#keyS = self.request.get('key', None);
 		#if keyS:
@@ -31,33 +32,41 @@ class DomainConsolidation(webapp.RequestHandler):
 		if memcache.get(memcache_key):
 			logging.info('getting from memcache %s ' % memcache.get('domain_update_key'))
 		  	key = memcache.get(memcache_key)
-			sessions = SessionModel.gql('WHERE date = :1 and __key__ > :2 ORDER by  __key__ asc ', date, key).fetch(limit=2)
+			if processType == 'all':
+				sessions = SessionModel.gql('WHERE __key__ > :1 ORDER by  __key__ asc ', key).fetch(limit=2)
+			else:
+				sessions = SessionModel.gql('WHERE date = :1 and __key__ > :2 ORDER by  __key__ asc ', date, key).fetch(limit=2)
 		else:
-			logging.info('getting from query')
-			sessions = SessionModel.gql('where date = :1 ORDER by __key__ asc ', date).fetch(limit=2)
+			if processType == 'all':
+				sessions = SessionModel.gql('ORDER by __key__ asc ').fetch(limit=2)
+				logging.info('getting from query: ORDER by __key__ asc')
+			else:	
+				sessions = SessionModel.gql('where date = :1 ORDER by __key__ asc ', date).fetch(limit=2)
+				logging.info('getting from query: where date = :1 ORDER by __key__ asc')
 		if len(sessions) < 2:
+			logging.info('one session caught')
 			# update last session domain
 			corrected_domain = ''
 			if len(sessions) == 1:
 				currentSession = sessions[0]
-				generalConsolidation.agregateData(currentSession, message)
+				self.agregateData(currentSession, message, processType)
 			# reset __key__ from cache
 			memcache.delete(memcache_key)
 			date = date + datetime.timedelta(days=1)
-			self.response.out.write('list out of range next date: %s' % str(date))
+			logging.info('list out of range next date: %s' % str(date))
+			#self.response.out.write('list out of range next date: %s' % str(date))
 			message = 'next date'
 			context = { 
 				'message': message,
 				'prev_url': '',
 				'prev_domain' : corrected_domain, 
-				'next_url' : '/correct_domains?date=%s' % str(date)
+				'next_url' : '/data_consolidation?date=%s&process_type=%s' % ( dateStr, processType)
 			}
 			path= os.path.join(os.path.dirname(__file__), 'templates/update_date.html')
 			self.response.out.write(template.render(path, context))
 			return
 		currentSession = sessions[0]
-		self.response.out.write('for date %s <br>' % currentSession.date)
-		generalConsolidation.agregateData(currentSession, message)
+		self.agregateData(currentSession, message, processType)
 
 		next_key = sessions[1].key()
 		memcache.set(memcache_key, next_key)
@@ -65,18 +74,20 @@ class DomainConsolidation(webapp.RequestHandler):
 			'message': message,
 			'prev_url': currentSession.url,
 			'prev_domain' : currentSession.domain, 
-			'next_url' : '/correct_domains?date=%s' % dateStr
+			'next_url' : '/data_consolidation?date=%s&process_type=%s' % ( dateStr, processType)
 		}
 		path= os.path.join(os.path.dirname(__file__), 'templates/update_date.html')
 		self.response.out.write(template.render(path, context))
-		
 			
-class GeneralConsolidation(webapp.RequestHandler):
-	def agregateData(self, currentSession, message):
-		logging.info('session date: %s ' % currentSession.date)
-		locationData =StatsUtil.ipResolverAPI(currentSession.ip)
+	def agregateData(self, currentSession, message, processType):
+		logging.info('agregate data for %s' % currentSession.date)
+		if processType == 'all':
+			locationData = None
+		else:
+			locationData =StatsUtil.ipResolverAPI(currentSession.ip)
 		message.append(currentSession.date)
 		if locationData and len(locationData) == 2:
+			logging.info('updating location data')
 			city= locationData[0]
 			countryCode = locationData[1]
 
@@ -117,7 +128,6 @@ class GeneralConsolidation(webapp.RequestHandler):
 				cityStat.put()
 				
 			
-		logging.info('link update')
 		existingLinkStat = LinkStats.gql('WHERE link = :1', currentSession.url).get()
 		if existingLinkStat:
 			existingLinkStat.count = existingLinkStat.count + 1
@@ -132,18 +142,29 @@ class GeneralConsolidation(webapp.RequestHandler):
 			linkStat.lastUpdatedBy = currentSession.instaright_account
 			linkStat.put()
 		# domain update
+		shouldUpdateSession = 0
+		mode = ''
 		if currentSession.domain is None or currentSession.domain == '':
 			currentSession.domain = StatsUtil.getDomain(currentSession.url)
-			logging.info('updating session %s ' % currentSession.domain)
-			message = 'updating session %s ' % currentSession.domain 
+			shouldUpdateSession = 1
+			mode='domain change: %s' % currentSession.domain
+		if currentSession.date is None or currentSession.date == '':
+			date = datetime.datetime.strptime('2009-11-15', '%Y-%m-%d').date() 
+			currentSession.date = date
+			shouldUpdateSession = 2
+			mode='date change: %s' % date
+		if shouldUpdateSession > 0:
+			message = 'updating session mode: %s' % mode
+			logging.info('updating session mode: %s' % mode)
 			currentSession.put()
 		else:
-			logging.info('existing domain: %s' % currentSession.domain)
-			message = 'existing domain: %s' % currentSession.domain
+			logging.info('unchanged session' )
+			message = 'unchanged session' 
+		logging.info('done data aggregation')
 		
 		
 application = webapp.WSGIApplication(
-                                     [('/data_consolidation', GeneralConsolidation ), ('/correct_domains',DomainConsolidation)],debug=True)
+                                     [('/data_consolidation',GeneralConsolidation)],debug=True)
 
 def main():
 	run_wsgi_app(application)
