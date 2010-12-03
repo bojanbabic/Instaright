55e491106b118df16a0e6fb46b3304dd844a6bdf
gbabun <gbabun@c8624efe-c7c6-11de-91be-c712730c8c03>
2010-12-03 13:45:10 +0000

    updating build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 cd $HOME/d/work/_ff_extensions/instaright/
+version=`grep "em:version" install.rdf  | sed -E  "s/.*>(.*)<.*/\\1/" `
+cat content/instaright/about.xul | sed -e "s/value=\"ver\.\ .*\"/value=\"ver\.\ $version\"/"  > content/instaright/about.xul.new
+mv content/instaright/about.xul.new content/instaright/about.xul
 zip -vr chrome/instaright.zip content/ skin/ locale/ -x "*.svn*"
 mv chrome/instaright.zip chrome/instaright.jar
-version=`grep "em:version" install.rdf  | sed -E  "s/.*>(.*)<.*/\\1/" `
 cat modules/AddonManager.jsm | sed -E "s/VERSION=.*/VERSION=\"$version\";/" > modules/AddonManager.jsm.new
 mv modules/AddonManager.jsm.new modules/AddonManager.jsm
 zip -vr instaright.zip chrome/ icon.png install.js  install.rdf chrome.manifest defaults/ modules/  -x "*.svn*"
