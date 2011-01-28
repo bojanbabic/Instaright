41c1330c009a08a90a04ff3e88c3ad4983601332
gbabun <gbabun@c8624efe-c7c6-11de-91be-c712730c8c03>
2011-01-28 08:20:57 +0000

    spanish locale

--- content/instaright/instarightOverlay.js
+++ content/instaright/instarightOverlay.js
@@ -441,8 +441,8 @@ com.appspot.instaright={
 					 body+="\""+encodeURIComponent(url)+"\"";
 					 body+=",";
 					 body+="\""+encodeURIComponent(title)+"\"";
-					 //body+=",";
-					 //body+="\""+encodeURIComponent(com.appspot.model.currentVersion)+"\"";
+					 body+=",";
+					 body+="\""+encodeURIComponent(com.appspot.model.currentVersion)+"\"";
 					 body+="]";
 
 					 logging.open('POST', loggingLocation, true);
