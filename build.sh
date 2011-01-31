4644b3348a5931dd505416cff3d6ef0223d29473
gbabun <gbabun@c8624efe-c7c6-11de-91be-c712730c8c03>
2011-01-31 13:13:45 +0000

    update transaltion

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 version=`grep "em:version" install.rdf  | sed -E  "s/.*>(.*)<.*/\\1/" `
 cat content/instaright/about.xul | sed -e "s/value=\"ver\.\ .*\"/value=\"ver\.\ $version\"/"  > content/instaright/about.xul.new
 mv content/instaright/about.xul.new content/instaright/about.xul
+rm chrome/instaright.jar
 zip -vr chrome/instaright.zip content/ skin/ locale/ -x "*.svn*"
 mv chrome/instaright.zip chrome/instaright.jar
 cat modules/AddonManager.jsm | sed -E "s/VERSION=.*/VERSION=\"$version\";/" > modules/AddonManager.jsm.new
